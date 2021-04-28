b76cbd7af9251a81b1bbc9ca85cbb3d65b390122
Peter Staker <peterjast@gmail.com>
2021-04-28 10:27:10 -0700

    adding proof of life route

--- src/auth/routes.js
+++ src/auth/routes.js
@@ -4,8 +4,12 @@ const express = require('express');
 const authRouter = express.Router();
 
 const User = require('./models/users.js');
-const basicAuth = require('./middleware/basic.js');
-const bearerAuth = require('./middleware/bearer.js');
+const basicAuth = require('./middleware/basic.js')
+const bearerAuth = require('./middleware/bearer.js')
+
+authRouter.get('/', (req, res) => {
+  res.status(200).send('Welcome to my bearer auth server');
+}) 
 
 authRouter.post('/signup', async (req, res, next) => {
   try {
